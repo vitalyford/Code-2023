b1e11b0bd2090acaab1d4d5e9bd0cb23e9bd9b08
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2023-09-13 19:56:26 -0400

    Add replace method, update comments

--- CustomLinkedList/LinkedList.java
+++ CustomLinkedList/LinkedList.java
@@ -20,7 +20,7 @@ class Node {
     }
 
     // public String toString() {
-    //     return item.toString();
+    // return item.toString();
     // }
 }
 
@@ -39,10 +39,12 @@ public class LinkedList {
 
     /**
      * This method returns an object provided the index.
-     * NOTE: it can support negative by applying the modulus operator to the index (if you want but you need to change the code).
+     * NOTE: it can support negative by applying the modulus operator to the index
+     * (if you want but you need to change the code).
+     * 
      * @param index an integer that describes the item in an indexed list
      *              that you would like to extract and get the value of.
-     * @return      the object found by the index.
+     * @return the object found by the index.
      * @throws Exception
      */
     public Object get(int index) throws Exception {
@@ -60,8 +62,7 @@ public class LinkedList {
             if (counter == index) {
                 return n.item;
             }
-        }
-        while (counter != index);
+        } while (counter != index);
 
         return null;
     }
@@ -69,25 +70,41 @@ public class LinkedList {
     // Add the end of the list
     public void add(Object obj) {
         // Verify if we already have an item in the list
-        if (size == 0 /*head.next == null */) {
+        if (size == 0 /* head.next == null */) {
             Node n = new Node(obj);
             head.next = n;
-        }
-        else { // Add the item if we already have stuff in the list
+        } else { // Add the item if we already have stuff in the list
             Node curr = head; // i = 0;
             while (curr.next != null) { // i != length() - 1
                 curr = curr.next; // i++;
             }
             // for (; curr.next != null; curr = curr.next){}
-            
+
             Node n = new Node(obj);
             curr.next = n;
         }
         size++;
     }
 
-    public void replace(Object obj, int index) {
+    public Object replace(int index, Object obj) throws Exception {
+        if (index < 0 || index >= size) {
+            throw new Exception("Dude, watch your index, you can do better than that. My code is bullet proof.");
+        }
+        // index = Math.floorMod(index, size); // Not needed anymore
+        int counter = -1;
+        Node n = head;
 
+        do {
+            counter++;
+            n = n.next;
+            if (counter == index) {
+                Object j = n.item;
+                n.item = obj;
+                return j;
+            }
+        } while (counter != index);
+
+        return null;
     }
 
     public static void main(String[] args) {
@@ -96,13 +113,14 @@ public class LinkedList {
         ll.add(1);
         ll.add(2);
         ll.add(3);
-        
+
         int index = -1;
         try {
             System.out.println(ll.get(index));
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            System.out.println("Recovered! Just a bad day.You gave me " + index + " but I do not support it. Sorry about that. No, not really sorry.");
+            System.out.println("Recovered! Just a bad day.You gave me " + index
+                    + " but I do not support it. Sorry about that. No, not really sorry.");
         }
 
         System.out.println("I am here!");
