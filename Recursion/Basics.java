cc6aadeee30f6e1863d45df9cea429e543d6593c
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2023-09-20 10:34:22 -0400

    Add rabbits with hasmaos

--- Recursion/Basics.java
+++ Recursion/Basics.java
@@ -1,5 +1,7 @@
 package Recursion;
 
+import java.util.*;
+
 public class Basics {
     public static long factorial(long n) { // 5! = 1 * 2 * 3 * 4 * 5 = 4! * 5
         return (n == 0) ? 1 : factorial(n - 1) * n;
@@ -19,8 +21,26 @@ public class Basics {
         return (message.length() <= 1) ? message : backwardString(message.substring(1)) + (message.charAt(0));
     }
 
+
+    public static int rabbits(int n, HashMap<Integer, Integer> memo) {
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        if (n < 3 ) {
+            return 1;
+        }
+        int value = rabbits(n - 1, memo) + rabbits(n - 2, memo);
+        memo.put(n, value);
+        System.out.println("For " + n + " it's " + value);
+        return value;
+    }
+
+
+
     public static void main(String[] args) {
         // System.out.println(factorial(20));
-        System.out.println(backwardString("A Santa lived as a devil at NASA"));
+        // System.out.println(backwardString("A Santa lived as a devil at NASA"));
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        System.out.println(rabbits(10, memo));
     }
 }
