377d8cdabee0676631b666d4a3917ce401bc2834
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2023-11-10 10:51:11 -0500

    Add hahsmap and comparator to PQ

--- PQ.java
+++ PQ.java
@@ -1,5 +1,35 @@
 import java.util.*;
 
+import javax.print.attribute.HashAttributeSet;
+
+
+class CompareByDavidsMethodZeroZero implements Comparator<Point> {
+    @Override
+    public int compare(Point p1, Point p2) {
+        return (p1.x*p1.x + p1.y*p1.y) - (p2.x*p2.x + p2.y*p2.y);
+    }
+}
+
+
+class ComparePointsByXandY implements Comparator<Point> {
+    @Override
+    public int compare(Point p1, Point p2) {
+        if (p1.x < p2.x) {
+            return 1;
+        }
+        if (p1.x == p2.x) {
+            if (p1.y < p2.y) {
+                return 1;
+            }
+        }
+        if (p1.x == p2.x && p1.y == p2.y) {
+            return 0;
+        }
+        return -1;
+    }
+}
+
+
 class Point implements Comparable<Point> {
     int x;
     int y;
@@ -32,7 +62,12 @@ class Point implements Comparable<Point> {
 
 public class PQ {
     public static void main(String[] args) {
-        PriorityQueue<Point> pq = new PriorityQueue<>();
+
+        HashMap<String, Comparator<Point>> comps = new HashMap<>();
+        comps.put("raffi", new ComparePointsByXandY());
+        comps.put("david", new CompareByDavidsMethodZeroZero());
+
+        PriorityQueue<Point> pq = new PriorityQueue<>(comps.get("raffi"));
 
         Random rand = new Random();
         for (int i = 0; i < 10; i++) {
